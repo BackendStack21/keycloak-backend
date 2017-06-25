12debb54d13cbcfb0b837e996d0eeb3434ffd591
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2017-06-25 16:33:14 +0200

    disabling loop

--- example.js
+++ example.js
@@ -1,21 +1,21 @@
 const config = require('./config-example');
-const kc = require('./index')(config);
+const keycloak = require('./index')(config);
 
 (async() => {
     try {
-        setInterval(async() => {
-            let start = new Date().getTime();
-            let info = await kc.users.info(await kc.accessToken.get());
-            let [details, roles] = await Promise.all([
-                kc.users.details(info.sub),
-                kc.users.roles(info.sub)
-            ]);
+        //setInterval(async() => {
+        let start = new Date().getTime();
+        let info = await keycloak.users.info(await keycloak.accessToken.get());
+        let [details, roles] = await Promise.all([
+            keycloak.users.details(info.sub),
+            keycloak.users.roles(info.sub)
+        ]);
 
-            console.log(new Date().getTime() - start);
-            console.log(info);
-            console.log(details);
-            console.log(roles);
-        }, 1000);
+        console.log(new Date().getTime() - start);
+        console.log(info);
+        console.log(details);
+        console.log(roles);
+        //}, 1000);
 
     } catch (err) {
         console.log(err);
