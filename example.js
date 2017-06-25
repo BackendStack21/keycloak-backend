88e5f8e91813de96ff36f0d7df8fca10c2e4ea27
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2017-06-25 17:25:59 +0200

    refactoring examples

--- example.js
+++ example.js
@@ -3,20 +3,22 @@ const keycloak = require('./index')(config);
 
 (async() => {
     try {
-        //setInterval(async() => {
-        let start = new Date().getTime();
+        // how to get openid info from access token...
+        // info.sub contains the user id
         let info = await keycloak.accessToken.info(await keycloak.accessToken.get());
+
+        // how to force service token refresh
+        await keycloak.accessToken.refresh(await keycloak.accessToken.get());
+
+        // how to get user details given the user id
         let [details, roles] = await Promise.all([
             keycloak.users.details(info.sub),
             keycloak.users.roles(info.sub)
         ]);
 
-        console.log(new Date().getTime() - start);
         console.log(info);
         console.log(details);
         console.log(roles);
-        //}, 1000);
-
     } catch (err) {
         console.log(err.response.status);
         console.log(err.response.statusText);
