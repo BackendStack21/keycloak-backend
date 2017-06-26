606ee5a04338f68c16cae689b576a6c1f213d295
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2017-06-26 14:41:03 +0200

    adding support for offline JWT accessToken verification

--- example.js
+++ example.js
@@ -1,5 +1,6 @@
 const config = require('./config-example');
 const keycloak = require('./index')(config);
+const fs = require('fs');
 
 (async() => {
     try {
@@ -8,8 +9,16 @@ const keycloak = require('./index')(config);
         // info.sub contains the user id
         let info = await keycloak.accessToken.info(someAccessToken);
 
-        // verify token, intended for micro-service authorization
+        // verify token online, intended for micro-service authorization
         let token = await keycloak.jwt.verify(someAccessToken);
+        //console.log(token.isExpired());
+        //console.log(token.hasRealmRole('user'));
+        //console.log(token.hasApplicationRole('nodejs-connect', 'vlm-readonly'));
+
+        // verify token offline, intended for micro-service authorization
+        // using this method does not consider token invalidation, avoid long-term tokens here
+        let cert = fs.readFileSync('public_cert.pem');
+        token = await keycloak.jwt.verifyOffline(someAccessToken, cert);
         //console.log(token.isExpired());
         //console.log(token.hasRealmRole('user'));
         //console.log(token.hasApplicationRole('nodejs-connect', 'vlm-readonly'));
@@ -24,6 +33,7 @@ const keycloak = require('./index')(config);
             keycloak.users.roles(info.sub)
         ]);
     } catch (err) {
+        console.log(err);
         console.log(err.response.status);
         console.log(err.response.statusText);
         console.log(err.response.data);
--- libs/Jwt.js
+++ libs/Jwt.js
@@ -1,10 +1,20 @@
 const axios = require('axios');
 const Token = require('./Token');
+const jwt = require('jsonwebtoken');
 
 class Jwt {
     constructor(config, request) {
         this.config = config;
         this.request = request;
+    }
+
+    verifyOffline(accessToken, cert, options = {}) {
+        return new Promise((resolve, reject) => {
+            jwt.verify(accessToken, cert, options, (err, payload) => {
+                if (err) reject(err);
+                resolve(new Token(accessToken));
+            });
+        });
     }
 
     async verify(accessToken) {
