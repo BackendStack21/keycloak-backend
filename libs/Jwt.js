d36bc46cbef289523d5f4111ce29735c94df7e92
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-03-20 11:47:08 +0100

    adding decode method

--- libs/Jwt.js
+++ libs/Jwt.js
@@ -1,31 +1,36 @@
-const axios = require('axios');
-const Token = require('./Token');
-const jwt = require('jsonwebtoken');
+const Token = require('./Token')
+const jwt = require('jsonwebtoken')
 
 class Jwt {
-    constructor(config, request) {
-        this.config = config;
-        this.request = request;
-    }
+  constructor (config, request) {
+    this.config = config
+    this.request = request
+  }
 
-    verifyOffline(accessToken, cert, options = {}) {
-        return new Promise((resolve, reject) => {
-            jwt.verify(accessToken, cert, options, (err, payload) => {
-                if (err) reject(err);
-                resolve(new Token(accessToken));
-            });
-        });
-    }
+  verifyOffline (accessToken, cert, options = {}) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(accessToken, cert, options, (err, payload) => {
+        if (err) reject(err)
+        resolve(new Token(accessToken))
+      })
+    })
+  }
 
-    async verify(accessToken) {
-        await this.request.get(`/auth/realms/${this.config.realm}/protocol/openid-connect/userinfo`, {
-            headers: {
-                'Authorization': 'Bearer ' + accessToken
-            }
-        });
+  decode (accessToken) {
+    return new Promise((resolve, reject) => {
+      resolve(new Token(accessToken))
+    })
+  }
 
-        return new Token(accessToken);
-    }
+  async verify (accessToken) {
+    await this.request.get(`/auth/realms/${this.config.realm}/protocol/openid-connect/userinfo`, {
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      }
+    })
+
+    return new Token(accessToken)
+  }
 }
 
-module.exports = Jwt;
+module.exports = Jwt
